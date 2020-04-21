639e8bfe0770bc7a51528aca076f710382c5f5ad
RomanPodgornyy <roman6773@gmail.com>
2020-04-21 19:52:34 +0300

    Adding base variables, set healthbars value and create attack method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,20 @@
+// Base constants
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 30;
 const MONSTER_ATTACK_VALUE = 15;
+
+// Base variables
+let maxHealth = 250;
+let currentMonsterHealth = maxHealth;
+let currentPlayerHealth = maxHealth;
+
+// Set healthbars value
+adjustHealthBars(maxHealth);
+
+// Attack monster
+function attackEvent() {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener('click', attackEvent);
