cc38d3ddd51a17f870d6fc49a7a89e3179bbd0a7
RomanPodgornyy <roman6773@gmail.com>
2020-04-22 00:16:46 +0300

    Adding new constants, create 'healEvent' method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,11 +2,14 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 30;
 const MONSTER_ATTACK_VALUE = 15;
+const HEAL_VALUE = 25;
+const HEAL_CHARGES = 3;
 
 // Base variables
-let maxHealth = 250;
+let maxHealth = 10;
 let currentMonsterHealth = maxHealth;
 let currentPlayerHealth = maxHealth;
+let healCounter = HEAL_CHARGES;
 
 // Set healthbars value
 adjustHealthBars(maxHealth);
@@ -30,10 +33,34 @@ function attackEvent(mode) {
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won!');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You lose!');
+        alert('You lose :(');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-        alert('You have a draw!');
+        alert('You have a draw! :O');
     }
+}
+
+// Heal player
+function healEvent() {
+    let healValue = HEAL_VALUE;
+
+    if (!healCounter) {
+        alert("You don't have charges for heal!");
+        return;
+    }
+
+    if (currentPlayerHealth == maxHealth) {
+        alert("You can't heal to more than your max initial health.");
+        return;
+    }
+
+    if (currentPlayerHealth >= maxHealth - HEAL_VALUE) {
+        healValue = maxHealth - currentPlayerHealth;
+    }
+
+    healCounter--;
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    alert(`You have ${healCounter} charges left.`); 
 }
 
 function baseAttack() {
@@ -47,3 +74,4 @@ function strongAttack() {
 // Event listeners
 attackBtn.addEventListener('click', baseAttack);
 strongAttackBtn.addEventListener('click', strongAttack);
+healBtn.addEventListener('click', healEvent);
